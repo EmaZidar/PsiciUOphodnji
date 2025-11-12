ba5b732e45c503657933042e8952990957dc9f78
LukaZadro <lukazadro987@gmail.com>
2025-11-12 22:43:59 +0100

    Spojio auth s backendom i frontendom

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,20 @@
+// ...existing code...
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header-container">
       <div className="header-inner">
-        <div className="brand"><a className="brand" href="/">Pawpal</a></div>
+        <div className="brand"><Link className="brand" to="/">Pawpal</Link></div>
         <nav className="header-nav" aria-label="Glavni izbornik">
           <a href="#about">O nama</a>
           <a href="#zasto">Zašto mi?</a>
           <a href="#kontakt">Kontakt</a>
-          <a href="/login" className="login-link">Prijava</a>
+          <Link to="/login" className="login-link">Prijava</Link>
         </nav>
       </div>
     </header>
   );
 }
+

c122e37d1dbcb8218a7eab4ad6aed29144161107
lana-sapic <sapic.lana@gmail.com>
2026-01-21 19:11:30 +0100

    popravila svoj bug + malo main page

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,13 +4,13 @@ export default function Steps() {
   return (
     <section className="steps-section">
       <div className="steps-inner">
-        <h3>Tri koraka do sretno umornog psa 🐕‍🦺</h3>
+        <h3>Tri koraka do sretnog psa </h3>
         <div className="steps-grid">
           <div className="step">
             <div className="step-icon">
               <img src="images/dog-1839808_1280.jpg" alt="dog" />
             </div>
-            <h4>1️⃣ Registriraj se</h4>
+            <h4>1. Registriraj se</h4>
             <p>Registriraj se kao vlasnik ili šetač</p>
           </div>
 
@@ -18,7 +18,7 @@ export default function Steps() {
             <div className="step-icon">
               <img src="images/phone.png" alt="phone" />
             </div>
-            <h4>2️⃣ Odaberi šetnju</h4>
+            <h4>2. Odaberi šetnju</h4>
             <p>Odaberi šetnju koja ti odgovara</p>
           </div>
 
@@ -26,7 +26,7 @@ export default function Steps() {
             <div className="step-icon">
               <img src="images/wallet.png" alt="wallet" />
             </div>
-            <h4>3️⃣ Plati i uživaj</h4>
+            <h4>3. Plati i uživaj</h4>
             <p>Plati i uživaj u slobodnom vremenu</p>
           </div>
         </div>
